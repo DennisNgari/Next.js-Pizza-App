505b95c02a7b4fe59ccccc102ff9be873155ee60
Dennis Ngari <ngaridennis3@gmail.com>
2022-01-24 19:33:16 +0300

    Updated the responsive design

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,9 +1,74 @@
 import styles from "../../styles/Orders.module.css"
+import Image from "next/image";
 
 const Order = () => {
+
+    const status = 0;
+    const statusClass = (index)=>{
+        if (index - status < 1) return styles.done
+        if (index - status === 1) return styles.inProgress
+        if (index - status > 1) return styles.unDone
+    }
     return (
         <div className={styles.container}>
-        <div className={styles.left}></div>
+        <div className={styles.left}>
+            <div className={styles.row}>
+            <table className={styles.table}>
+                <tr className={styles.tr}>
+                    <th>Order ID</th>
+                    <th>Customer</th>
+                    <th>Address</th>
+                    <th>Total</th>
+                </tr>
+                <tr>
+                    <td>
+                        <span className={styles.id}>12345</span>
+                    </td>
+                    <td>
+                        <span className={styles.name}>Jane Doe</span>
+                    </td>
+                    <td>
+                        <span className={styles.address}> Nax Vegas st. 234</span>
+                    </td>
+                    <td>
+                        <span className={styles.total}> $39.80 </span>
+                    </td>
+                </tr>
+                
+            </table>
+            </div>
+            <div className={styles.row}>
+                <div className={statusClass(0)}>
+                    <Image src="/img/paid.png"  alt="" width={30} height={30}/>
+                    <span>Payment</span>
+                    <div className={styles.checkedIcon}>
+                    <Image src="/img/checked.png"  alt="" width={20} height={20}/>
+                    </div>
+                </div>
+                <div className={statusClass(1)}>
+                    <Image src="/img/bake.png"  alt="" width={30} height={30}/>
+                    <span>Preparing</span>
+                    <div className={styles.checkedIcon}>
+                    <Image  src="/img/checked.png"  alt="" width={20} height={20}/>
+                    </div>
+                </div>
+                <div className={statusClass(2)}>
+                    <Image src="/img/bike.png"  alt="" width={30} height={30}/>
+                    <span>On Transit</span>
+                    <div className={styles.checkedIcon}>
+                    <Image  src="/img/checked.png"  alt="" width={20} height={20}/>
+                    </div>
+                </div>
+                <div className={statusClass(3)}>
+                    <Image src="/img/delivered.png"  alt="" width={30} height={30}/>
+                    <span>Delivered</span>
+                    <div className={styles.checkedIcon}>
+                    <Image  src="/img/checked.png"  alt="" width={20} height={20}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div className={styles.right}>
         <div className={styles.wrapper}>
                 <h2 className={styles.title}> CART TOTAL</h2>
